c4b5d7c178228038d52d133613e8aeb899760e90
shin <shin.php@gmail.com>
2012-07-30 11:30:42 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Combat::Application.config.secret_token = '498572e780da718460a43bd8dd162ddc67c9934ac4248d0fce07a1c3697f148831b60d37f972c0c6cef327c49c5da84324e8334b2c3e6ea94062fbea77158bb2'
